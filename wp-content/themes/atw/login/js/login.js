deec95a1d80556b5891a50d1002cb3d21b92bc2d
khalvin <you@example.com>
2018-05-11 13:47:38 +0800

    login page 95% done

--- wp-content/themes/atw/login/js/login.js
+++ wp-content/themes/atw/login/js/login.js
@@ -1,6 +1,6 @@
 jQuery(function($){
 
-console.log(login_images.theme_path + "1.jpg" );
+	$('body.login div#login p#nav, body.login div#login p#backtoblog').wrapAll('<div class="bottom"></div>');
 
 	$('body').vegas({
 
@@ -8,7 +8,13 @@ console.log(login_images.theme_path + "1.jpg" );
 			{ src: login_images.theme_path + "1.jpg" },
 			{ src: login_images.theme_path + "2.jpg" },
 			{ src: login_images.theme_path + "3.jpg" }
-		]
+		],
+		overlay: login_images.theme_path + 'overlays/08.png',
+		transition: ['blur','swirlLeft','zoomOut','blur2'],
+		animation: 'random',
+		transitionDuration: 3000,
+		delay:7000,
+
 	})
 
 });
